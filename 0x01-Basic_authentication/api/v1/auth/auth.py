0ee69f9d3d6717bac811905d0fb0a99dff11af5a
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-07 09:11:50 +0100

    Add BasicAuth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -22,7 +22,11 @@ class Auth():
 
     def authorization_header(self, request=None) -> str:
         """authorization_header method"""
-        return None
+        if request is None or "Authorization" not in request.headers:
+            return None
+        else:
+            return request.headers.get("Authorization")
+
 
     def current_user(self, request=None) -> TypeVar('User'):
         """current_user"""
