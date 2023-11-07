50b5d031073d704fab2d2acfa610b8f4d225dd9f
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-07 08:24:13 +0100

    validate all requests to secure the API

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,17 +19,19 @@ if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request():
+    """Runs before request"""
     if auth is None:
         pass
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
     if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) == None:
+        if auth.authorization_header(request) is None:
             abort(401)
-        if auth.current_user(request) == None:
+        if auth.current_user(request) is None:
             abort(403)
-
 
 
 @app.errorhandler(404)
