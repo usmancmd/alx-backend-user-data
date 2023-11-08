5c042a1b5d2e4c03f5153c58b5222194ccbb6f07
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-08 17:53:51 +0100

    fix some bugs

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -29,9 +29,11 @@ def before_request():
     if auth is None:
         pass
     else:
-        excluded_paths = ['/api/v1/status/',
-                        '/api/v1/unauthorized/',
-                        '/api/v1/forbidden/']
+        excluded_paths = [
+                            '/api/v1/status/',
+                            '/api/v1/unauthorized/',
+                            '/api/v1/forbidden/'
+                        ]
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
