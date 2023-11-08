073ec72ad5c6a85616ea3ca21a75550590937e4d
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-08 17:50:00 +0100

    fix some bugs

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -28,13 +28,15 @@ def before_request():
     """Runs before request"""
     if auth is None:
         pass
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    else:
+        excluded_paths = ['/api/v1/status/',
+                        '/api/v1/unauthorized/',
+                        '/api/v1/forbidden/']
+        if auth.require_auth(request.path, excluded_paths):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
