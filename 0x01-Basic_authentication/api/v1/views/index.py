446ead5ce686b76ff423dccb77a09466f16467ae
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-08 07:52:28 +0100

    Add comments to unauthorized error handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -28,6 +28,7 @@ def stats() -> str:
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized():
+    """Abort unauthorized request"""
     return abort(401)
 
 
