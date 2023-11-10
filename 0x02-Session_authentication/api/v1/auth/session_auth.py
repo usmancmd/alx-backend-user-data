b5adad0d31bac283bc55b93fc4ca609185dfd78c
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-10 18:17:26 +0100

    Add solution to task-2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,8 +4,13 @@
 from api.v1.auth.auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     """SessionAuth Class"""
+    def __init__(self):
+        """Session Initialization"""
+        super().__init__()
+
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
@@ -14,7 +19,6 @@ class SessionAuth(Auth):
             return None
         if type(user_id) is not str:
             return None
-        session_id = uuid.uuid4()
+        session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
-
