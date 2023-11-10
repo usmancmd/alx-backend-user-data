9c2e6fd6322c648c26ea2b2352fe664c7c403d00
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-10 21:50:06 +0100

    Add solution to task-4

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -44,4 +45,5 @@ class Auth():
         """Returns a cookie value from a request"""
         if request is None:
             return None
-        return request.cookies.get("_my_session_id")
+        cookie_name = os.getenv("SESSION_NAME")
+        return request.cookies.get(cookie_name)
