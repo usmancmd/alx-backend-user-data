156c4f3b0e6273a187122beb2861f43115f2c305
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-10 20:52:57 +0100

    Add solution to task-3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -22,3 +22,11 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Returns a User ID based on a Session ID"""
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
