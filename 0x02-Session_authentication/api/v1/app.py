2a49e03dd62faf09f70f92e5d58b0f1ea88fe7d6
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-13 04:02:55 +0100

    Add solution to task-7

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,24 +28,24 @@ if AUTH_TYPE == "session_auth":
 
 @app.before_request
 def before_request():
-    """Runs before request"""
+    """Runs before any other request"""
     if auth is None:
-        pass
-    else:
-        excluded_paths = [
+        return
+    excluded_paths = [
                             '/api/v1/status/',
                             '/api/v1/unauthorized/',
                             '/api/v1/forbidden/',
                             '/api/v1/auth_session/login/'
-                         ]
-        if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request)\
-                    is None and auth.session_cookie(request) is None:
-                abort(401)
+                     ]
+    if auth.require_auth(request.path, excluded_paths) is False:
+        return
 
-            if auth.current_user(request) is None:
-                abort(403)
-            request.current_user = auth.current_user(request)
+    if ((auth.authorization_header(request) is None) and (auth.session_cookie(request) is None)):  # noqa: E501
+        abort(401)
+
+    if (auth.current_user(request) is None):
+        abort(403)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
