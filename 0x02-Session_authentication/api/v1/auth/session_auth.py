a4c1b619c3023e2bfec574ea46239c5c2c387132
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-13 05:00:29 +0100

    Add solution to task-8

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -39,3 +39,16 @@ class SessionAuth(Auth):
 
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Destroys user session"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,14 +2,14 @@
 """
 Flask view that handles all routes for the Session authentication
 """
-from flask import request, jsonify
+from flask import request, jsonify, abort
 from api.v1.views import app_views
 from models.user import User
 import os
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def auth_session():
+def login():
     """session auth"""
     email = request.form.get("email")
     password = request.form.get("password")
@@ -28,3 +28,12 @@ def auth_session():
         user_json.set_cookie(session_name, session_id)
         return user_json
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('auth_session/logout', methods=['DELETE'], strict_slashes=False)  # noqa E502
+def logout():
+    """logout user"""
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({}), 200
