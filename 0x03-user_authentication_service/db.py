f795567bf254b3c3a9eb4b547f42af6bf925d93b
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-14 11:06:50 +0100

    Add solution to task-1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm.session import Session
 
 from user import Base, User
 
-session = Session()
+
 class DB:
     """DB class
     """
@@ -15,7 +15,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -29,9 +29,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add new user"""
-         user = User(email, hashed_password)
-         session.add(user)
-         session.commit()
-         return user
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
