b45c6fdb5b59a314d7658586f38d595088814b63
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-15 09:07:35 +0100

    Add solution to task-11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """Flask app"""
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
+from sqlalchemy.orm.exc import NoResultFound
 from auth import Auth
 
 AUTH = Auth()
@@ -24,6 +25,21 @@ def users():
         return jsonify({"email": f"{email}", "message": "user created"})  # noqa E502
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Login user"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    response = jsonify({"email": f"{email}", "message": "logged in"})
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
