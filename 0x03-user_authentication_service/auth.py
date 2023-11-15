51751a90e99517ec576722aaa74047c085a679f9
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-15 09:19:06 +0100

    Add solution to task-12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,7 @@ from user import User
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -53,5 +54,15 @@ class Auth:
                 session_id = _generate_uuid()
                 self._db.update_user(user.id, session_id=session_id)
                 return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Union[User | None]:
+        """Get user from session id"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except NoResultFound:
             return None
