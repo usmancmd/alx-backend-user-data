73854d6f4c55d17bf8934851c11f059bf2971669
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-15 12:33:50 +0100

    Add solution to task-10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,9 +52,9 @@ class Auth:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
-            return session_id
         except NoResultFound:
             return None
+        return session_id
 
     def get_user_from_session_id(self, session_id: str) -> Union[User | None]:
         """Get user from session id"""
