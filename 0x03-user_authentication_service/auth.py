a1b3f940ce8e987dadad3980bb4b7e933960e95d
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-15 15:31:07 +0100

    Add solution to task-10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,10 +47,12 @@ class Auth:
             return False
 
     def create_session(self, email: str) -> str:
-        """Create session for a user"""
+        """Create session for a user and returns the session id"""
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
+            return None
+        if user is None:
             return None
         session_id = _generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
