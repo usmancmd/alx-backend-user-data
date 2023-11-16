c5a282d5996d8d8baaed479741883fab68421614
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-16 15:50:51 +0100

    Add solution to task-18

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,13 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            password_hash = _hash_password(password)
+            setattr(user, user.password_hash, password_hash)
+            setattr(user, reset_token, None)
+        except NoResultFound:
+            raise ValueError:
