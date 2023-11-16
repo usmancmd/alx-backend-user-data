797531a17ce9b100e6b3f0f84e23550823fdfe3d
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-16 11:10:41 +0100

    Add solution to task-14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -48,7 +48,7 @@ def login():
 def logout():
     """Logout user"""
     session_id = request.cookies.get("session_id")
-    user = DB.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
     if user is not None:
         AUTH.destroy_session(user.id)
         return redirect("/")
