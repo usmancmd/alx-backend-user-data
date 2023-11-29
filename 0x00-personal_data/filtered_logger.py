b71ccfbe23a3511012c53ae6f5d8b57b4573d7db
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-29 15:39:43 +0100

    Add solution to task-1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str],
@@ -13,3 +14,21 @@ def filter_datum(fields: List[str],
     for field in fields:
         message = re.sub(field+'=.*?'+separator, field+'='+redaction+separator, message)  # noqa E502
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        redacted_msg = filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)  # noqa E502
+        return redacted_msg
