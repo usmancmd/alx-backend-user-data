001e1690fac3eed7fa50d68faafa073e1d55803b
Usman Adepoju <adepojuusman465@gmail.com>
2023-11-29 15:26:56 +0100

    Add solution task-0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,13 +2,14 @@
 """filter_datum that returns the log message obfuscated"""
 
 import re
+from typing import List
 
 
-def filter_datum(fields: str,
+def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
                  separator: str) -> str:
     """returns the log message obfuscated"""
-    pw = r'(?<=password=)[a-z]+'
-    dt = r'(?<=date_of_birth=)\d{2}/\d{2}/\d{4}'
-    return re.sub(pw, redaction, re.sub(dt, redaction, message))
+    for field in fields:
+        message = re.sub(field+'=.*?'+separator, field+'='+redaction+separator, message)  # noqa E502
+    return message
